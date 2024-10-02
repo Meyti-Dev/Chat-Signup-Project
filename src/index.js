6749d73c61c40223fa2bfa9d94aae7cba7b19254
mahdi ramezani <mahdiramezani@gmail.com>
2024-10-02 15:35:20 +0330

    updated routers

--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
 // dependencies
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // styles
-import './index.css';
+import "./index.css";
 // pages
-import App from './Components/App';
-import AddUser from './Components/AddUser/AddUser';
-import Users from './Components/User/Users';
-import ViewUser from './Components/ViewUser/ViewUser';
-import EditUser from './Components/EditUser/EditUser.jsx'
+import App from "./Components/App";
+import AddUser from "./Components/AddUser/AddUser";
+import Users from "./Components/User/Users";
+import ViewUser from "./Components/ViewUser/ViewUser";
+import EditUser from "./Components/EditUser/EditUser.jsx";
+
+// routes
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Users />,
+      },
+      {
+        path: "/adduser",
+        element: <AddUser />,
+      },
+      {
+        path: "/view/:ID",
+        element: <ViewUser />,
+      },
+      {
+        path: "/edit/:ID",
+        element: <EditUser />,
+      },
+    ],
+  },
+]);
 
 // rendering
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-          <Route path='/' element={<Users />} />
-          <Route path='/adduser' element={<AddUser />} />
-          <Route path='/view/:ID' element={<ViewUser />} />
-          <Route path='/edit/:ID' element={<EditUser />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={routes} />
   </StrictMode>
 );
